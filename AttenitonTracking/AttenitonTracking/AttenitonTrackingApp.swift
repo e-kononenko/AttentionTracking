1161f4fd3df6fff65f63bb038d91608d7f59d641
e-kononenko <yevgeny,kononenko@>
2025-07-04 12:16:02 +0200

    Initial project

---
+++ AttenitonTracking/AttenitonTracking/AttenitonTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttenitonTrackingApp.swift
+//  AttenitonTracking
+//
+//  Created by Evgenii Kononenko on 04.07.25.
+//
+
+import SwiftUI
+
+@main
+struct AttenitonTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AttenitonTracking/AttenitonTrackingTests/AttenitonTrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  AttenitonTrackingTests.swift
+//  AttenitonTrackingTests
+//
+//  Created by Evgenii Kononenko on 04.07.25.
+//
+
+import Testing
+@testable import AttenitonTracking
+
+struct AttenitonTrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
