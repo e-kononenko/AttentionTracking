50a59fe750fb18bf582258b08a7de472f7bd35e1
e-kononenko <yevgeny.kononenko@gmail.com>
2025-07-04 18:54:49 +0200

    cleanup in VisibilityHelper

--- AttenitonTracking/AttenitonTracking/VisibilityHelper.swift
+++ AttenitonTracking/AttenitonTracking/VisibilityHelper.swift
@@ -7,36 +7,6 @@
 import Foundation
 
 enum VisibilityHelper {
-//    static func findVisibleAndDisappearedIds(
-//        idFrames: [Int : CGRect],
-//        inParentBounds parentBounds: CGRect
-//    ) -> (visibleIds: Set<Int>, disappearedIds: Set<Int>) {
-//        var visibleIds: Set<Int> = .init()
-//        var disappearedIds: Set<Int> = .init()
-//        
-//        for idFrame in idFrames {
-//            let id = idFrame.key
-//            let frame = idFrame.value
-//            if isFrameVisible(frame, inParentBounds: parentBounds) {
-//                visibleIds.insert(id)
-//            } else if isFrameDisappeared(frame, inParentBounds: parentBounds) {
-//                disappearedIds.insert(id)
-//            }
-//        }
-//        print("Visible ids: \(visibleIds)\nDisappeared ids: \(disappearedIds)")
-//        return (visibleIds, disappearedIds)
-//    }
-//
-//    static func isFrameVisible(_ frame: CGRect, inParentBounds parentBounds: CGRect) -> Bool {
-//        // consider only fully visible items in this example
-//        frame.minY >= parentBounds.minY && frame.maxY <= parentBounds.maxY
-//    }
-//
-//    static func isFrameDisappeared(_ frame: CGRect, inParentBounds parentBounds: CGRect) -> Bool {
-//        // if we don't see it anymore, we consider it as disappeared
-//        !frame.intersects(parentBounds)
-//    }
-
     // Frames can be partially visible.
     // Small parts of other frames can be shown at top and at bottom.
     // We don't want to consider them as visible.
