d732cd47c5be00cfbe733fc20a7181f5d91db12c
e-kononenko <yevgeny.kononenko@gmail.com>
2025-07-07 01:18:48 +0200

    adjusted VisibilityHelper

--- AttenitonTracking/AttentionTracking/VisibilityHelper.swift
+++ AttenitonTracking/AttentionTracking/VisibilityHelper.swift
@@ -10,15 +10,11 @@ enum VisibilityHelper {
     static func isItemFrameVisible(
         _ frame: CGRect,
         inParentFrame parentFrame: CGRect,
-        visibilityThreshold: Double = 1.0 // for partially visible items (1.0 - fully visible, 0.5 - visible at 50%)
+        visibilityThreshold: Double = 1.0 // for partially visible items, e.g 0.7 - visible at 70%
     ) -> Bool {
         guard frame.height > 0 else { return false }
 
-        let intersectionTop = max(frame.minY, parentFrame.minY)
-        let intersectionBottom = min(frame.maxY, parentFrame.maxY)
-
-        let visibleHeight = max(0, intersectionBottom - intersectionTop)
-
+        let visibleHeight = frame.intersection(parentFrame).height
         let visibility = visibleHeight / frame.height
 
         return visibility >= visibilityThreshold
